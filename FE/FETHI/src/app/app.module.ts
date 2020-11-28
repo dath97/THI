e451bf420affc0b2fac3eaa849e41636228831cc
dath97 <danhthua99@gmail.com>
2020-11-28 14:04:27 +0700

    add

--- FE/FETHI/src/app/app.module.ts
+++ FE/FETHI/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './Common/header/header.component';
+import { HomeComponent } from './Component/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
